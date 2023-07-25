aee7f9b3251593c13d2e569b1fc52acddf952c14
diamondburned <diamond@arikawa-hi.me>
2023-07-24 23:39:32 -0700

    tree-canvas: streamline output flags for consistency

--- cmd/tree-canvas/main.go
+++ cmd/tree-canvas/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/csv"
 	"fmt"
 	"image"
 	"image/color"
@@ -8,6 +10,7 @@ import (
 	"image/png"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -28,7 +31,10 @@ import (
 
 var (
 	ledPointsFile = "led-points.csv"
-	outFile       = "led-image.png"
+	outputDir     = "."
+	pngImageFile  = ""
+	csvColorFile  = ""
+	goCodeFile    = ""
 	maxPtDistance = 0.0 // auto
 	ppi           = 72.0
 	fit           = false
@@ -36,7 +42,10 @@ var (
 
 func init() {
 	pflag.StringVarP(&ledPointsFile, "led-points", "i", ledPointsFile, "path to the CSV file containing the LED points")
-	pflag.StringVarP(&outFile, "out", "o", outFile, "path to the output PNG file")
+	pflag.StringVarP(&outputDir, "output-dir", "o", outputDir, "path to the output directory")
+	pflag.StringVar(&pngImageFile, "png-image", pngImageFile, "path to the output PNG image file")
+	pflag.StringVar(&csvColorFile, "csv-color", csvColorFile, "path to the output CSV color file")
+	pflag.StringVar(&goCodeFile, "go-code", goCodeFile, "path to the output Go code file")
 	pflag.Float64Var(&maxPtDistance, "max-distance", maxPtDistance, "maximum distance between a point and an LED")
 	pflag.Float64Var(&ppi, "ppi", ppi, "pixels per inch")
 	pflag.BoolVar(&fit, "fit", fit, "fill or fit the source image (default: fill)")
@@ -44,10 +53,6 @@ func init() {
 
 func main() {
 	pflag.Parse()
-
-	if !strings.HasSuffix(outFile, ".png") {
-		log.Fatalln("output file must be a PNG")
-	}
 
 	ledPoints, err := readCSVPoints(ledPointsFile)
 	if err != nil {
@@ -67,13 +72,6 @@ func main() {
 
 	// TODO: scale while preserving aspect ratio, and center the image.
 	canvasBounds := ledCanvas.CanvasBounds()
-
-	if pflag.Arg(0) == "" {
-		log.Printf("Debug Information:")
-		log.Printf("  LED bounds:  %v", ledCanvas.LEDBounds())
-		log.Printf("  Canvas size: %dx%d", canvasBounds.Dx(), canvasBounds.Dy())
-		return
-	}
 
 	img, err := decodeImageFile(pflag.Arg(0))
 	if err != nil {
@@ -96,6 +94,35 @@ func main() {
 	}
 	log.Println("rendered in", time.Since(start))
 
+	if pngImageFile != "" {
+		if err := writePNGImage(ledCanvas, ledPoints); err != nil {
+			log.Fatalln("failed to write PNG image:", err)
+		}
+	}
+
+	if csvColorFile != "" {
+		if err := writeCSVColors(ledCanvas, ledPoints); err != nil {
+			log.Fatalln("failed to write CSV colors:", err)
+		}
+	}
+
+	if goCodeFile != "" {
+		if err := writeGoCode(ledCanvas, ledPoints); err != nil {
+			log.Fatalln("failed to write Go code:", err)
+		}
+	}
+
+	if pngImageFile == "" && csvColorFile == "" && goCodeFile == "" {
+		log.Println("Nothing to do.")
+		log.Println()
+		log.Println("Debug Information:")
+		log.Println("  LED bounds:  ", ledCanvas.LEDBounds())
+		log.Println("  Canvas size: ", canvasBounds.Dx(), "x", canvasBounds.Dy())
+		log.Println()
+	}
+}
+
+func writePNGImage(ledCanvas *leddraw.LEDCanvas, ledPoints []image.Point) error {
 	outImage := image.NewRGBA(ledCanvas.LEDBounds())
 	draw.Draw(outImage, outImage.Bounds(), image.NewUniform(color.Black), image.ZP, draw.Src)
 
@@ -105,21 +132,89 @@ func main() {
 		xdraw.DrawCircle(outImage, ledPoints[i], 3, led)
 	}
 
-	out, err := os.Create(outFile)
+	f, err := createFile(pngImageFile)
 	if err != nil {
-		log.Fatalln("failed to open output file:", err)
+		return fmt.Errorf("failed to create output file: %v", err)
 	}
-	defer out.Close()
+	defer f.Close()
 
-	if err := png.Encode(out, outImage); err != nil {
-		log.Fatalln("failed to encode PNG:", err)
-	}
-
-	if err := out.Close(); err != nil {
-		log.Fatalln("failed to close output file:", err)
+	if err := png.Encode(f, outImage); err != nil {
+		return fmt.Errorf("failed to encode PNG: %v", err)
 	}
 
-	log.Println("wrote output to", outFile)
+	if err := f.Close(); err != nil {
+		return fmt.Errorf("failed to close output file: %v", err)
+	}
+
+	return nil
+}
+
+func writeCSVColors(ledCanvas *leddraw.LEDCanvas, ledPoints []image.Point) error {
+	type color struct {
+		R, G, B uint8
+	}
+
+	colors := make([]color, len(ledCanvas.LEDs()))
+	for i, led := range ledCanvas.LEDs() {
+		colors[i] = color{led.R, led.G, led.B}
+	}
+
+	f, err := createFile(csvColorFile)
+	if err != nil {
+		return fmt.Errorf("failed to create output file: %v", err)
+	}
+	defer f.Close()
+
+	if err := csvutil.Marshal(csv.NewWriter(f), colors); err != nil {
+		return fmt.Errorf("failed to marshal CSV: %v", err)
+	}
+
+	if err := f.Close(); err != nil {
+		return fmt.Errorf("failed to close output file: %v", err)
+	}
+
+	return nil
+}
+
+func writeGoCode(ledCanvas *leddraw.LEDCanvas, ledPoints []image.Point) error {
+	var buf bytes.Buffer
+	buf.WriteString("var ledColors = []color.RGBA{")
+	for _, led := range ledCanvas.LEDs() {
+		buf.WriteString(fmt.Sprintf("{0x%02X, 0x%02X, 0x%02X, 0xFF}, ", led.R, led.G, led.B))
+	}
+	buf.WriteString("}\n")
+
+	f, err := createFile(goCodeFile)
+	if err != nil {
+		return fmt.Errorf("failed to create output file: %v", err)
+	}
+	defer f.Close()
+
+	if _, err := buf.WriteTo(f); err != nil {
+		return fmt.Errorf("failed to write to output file: %v", err)
+	}
+
+	if err := f.Close(); err != nil {
+		return fmt.Errorf("failed to close output file: %v", err)
+	}
+
+	return nil
+}
+
+func createFile(name string) (*os.File, error) {
+	if name == "-" {
+		return os.Stdout, nil
+	}
+
+	if err := os.MkdirAll(filepath.Dir(outputDir), 0755); err != nil {
+		return nil, fmt.Errorf("failed to create output directory: %v", err)
+	}
+
+	if strings.HasPrefix(name, "/") {
+		return os.Create(name)
+	}
+
+	return os.Create(filepath.Join(outputDir, name))
 }
 
 func readCSVPoints(csvPath string) ([]image.Point, error) {
