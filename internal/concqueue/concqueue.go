a36f7afd815f1c62606776fe4f9095132ec955df
diamondburned <diamond@arikawa-hi.me>
2023-08-28 18:37:08 -0700

    Allow concqueue processing errors

--- internal/concqueue/concqueue.go
+++ internal/concqueue/concqueue.go
@@ -6,26 +6,64 @@ import (
 	"sync/atomic"
 )
 
+// Result is the result of a single queue operation.
+type Result[T any] struct {
+	v   T
+	err error
+}
+
+// Ok returns a successful result.
+func Ok[T any](v T) Result[T] {
+	return Result[T]{v: v}
+}
+
+// Error returns an error result.
+func Error[T any](err error) Result[T] {
+	return Result[T]{err: err}
+}
+
+// NoResult returns a result that indicates that there is no result. This is
+// useful for queues that do not return a result.
+func NoResult[T any]() Result[T] {
+	return Result[T]{err: errNoResult}
+}
+
+var errNoResult = errors.New("no result")
+
+// Value returns the value of the result. It panics if the result is an error.
+func (r *Result[T]) Value() T {
+	if r.err != nil {
+		panic(r.err)
+	}
+	return r.v
+}
+
+// Error returns the error of the result. It returns nil if the result is not an
+// error.
+func (r *Result[T]) Error() error {
+	return r.err
+}
+
 // Queue implements a concurrent first-in-first-out queue. Elements are
 // processed in the order they are added.
 type Queue[InputT, OutputT any] struct {
 	In  chan<- InputT
-	Out <-chan OutputT
+	Out <-chan Result[OutputT]
 
 	inputs  chan InputT
-	outputs chan OutputT
+	outputs chan Result[OutputT]
 	doFunc  DoFunc[InputT, OutputT]
 	running atomic.Bool
 }
 
 // DoFunc is the function that is called for each element in the queue.
-type DoFunc[InputT, OutputT any] func(context.Context, InputT) OutputT
+type DoFunc[InputT, OutputT any] func(context.Context, InputT) (OutputT, error)
 
 // NewQueue creates a new Queue.
 func NewQueue[InputT, OutputT any](doFunc DoFunc[InputT, OutputT]) *Queue[InputT, OutputT] {
 	q := &Queue[InputT, OutputT]{
 		inputs:  make(chan InputT),
-		outputs: make(chan OutputT, 1),
+		outputs: make(chan Result[OutputT], 1),
 		doFunc:  doFunc,
 	}
 	q.In = q.inputs
@@ -43,11 +81,11 @@ func (q *Queue[InputT, OutputT]) Run(ctx context.Context) error {
 		case <-ctx.Done():
 			return ctx.Err()
 		case input := <-q.inputs:
-			out := q.doFunc(ctx, input)
+			out, err := q.doFunc(ctx, input)
 			select {
 			case <-ctx.Done():
 				return ctx.Err()
-			case q.outputs <- out:
+			case q.outputs <- Result[OutputT]{v: out, err: err}:
 			}
 		}
 	}
@@ -66,39 +104,43 @@ func (q *Queue[InputT, OutputT]) Enqueue(ctx context.Context, v ...InputT) error
 	return nil
 }
 
+// EnqueueList adds a list of elements to the queue. If the queue is full, the
+// function blocks until there is room for the elements.
+func (q *Queue[InputT, OutputT]) EnqueueList(ctx context.Context, vs []InputT) error {
+	return q.Enqueue(ctx, vs...)
+}
+
 // Dequeue removes an element from the queue. If the queue is empty, the
 // function blocks until there is an element.
-func (q *Queue[InputT, OutputT]) Dequeue(ctx context.Context) (OutputT, error) {
+func (q *Queue[InputT, OutputT]) Dequeue(ctx context.Context) (Result[OutputT], error) {
 	select {
 	case <-ctx.Done():
-		var z OutputT
-		return z, ctx.Err()
+		return Result[OutputT]{err: errNoResult}, ctx.Err()
 	case output := <-q.outputs:
 		return output, nil
 	}
 }
 
 // TryDequeue removes an element from the queue. If the queue is empty, the
-// function returns immediately.
-func (q *Queue[InputT, OutputT]) TryDequeue() (OutputT, bool) {
+// function returns immediately with a zero value.
+func (q *Queue[InputT, OutputT]) TryDequeue() (Result[OutputT], bool) {
 	select {
 	case output := <-q.outputs:
 		return output, true
 	default:
-		var z OutputT
-		return z, false
+		return NoResult[OutputT](), false
 	}
 }
 
 // DequeueN removes n elements from the queue. If the queue is empty, the
 // function blocks until there are n elements.
-func (q *Queue[InputT, OutputT]) DequeueN(ctx context.Context, n int) ([]OutputT, error) {
-	outputs := make([]OutputT, 0, n)
+func (q *Queue[InputT, OutputT]) DequeueN(ctx context.Context, n int) ([]Result[OutputT], error) {
+	outputs := make([]Result[OutputT], 0, n)
 	for i := 0; i < n; i++ {
 		select {
 		case <-ctx.Done():
-			return outputs, ctx.Err()
-		case output := <-q.outputs:
+			return nil, ctx.Err()
+		case output := <-q.Out:
 			outputs = append(outputs, output)
 		}
 	}
--- internal/concqueue/concqueue_test.go
+++ internal/concqueue/concqueue_test.go
@@ -12,8 +12,8 @@ func TestQueue(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	t.Cleanup(cancel)
 
-	q := NewQueue(func(_ context.Context, v int) string {
-		return strconv.Itoa(v)
+	q := NewQueue(func(_ context.Context, v int) (string, error) {
+		return strconv.Itoa(v), nil
 	})
 	go func() { q.Run(ctx) }()
 
@@ -25,14 +25,14 @@ func TestQueue(t *testing.T) {
 
 	items, err := q.DequeueN(ctx, 3)
 	assert.NoError(t, err)
-	assert.Equal(t, []string{"1", "2", "3"}, items)
+	assert.Equal(t, []Result[string]{Ok("1"), Ok("2"), Ok("3")}, items)
 }
 
 func BenchmarkQueue(b *testing.B) {
 	ctx, cancel := context.WithCancel(context.Background())
 	b.Cleanup(cancel)
 
-	q := NewQueue(func(_ context.Context, v int) int { return v })
+	q := NewQueue(func(_ context.Context, v int) (int, error) { return v, nil })
 	go func() { q.Run(ctx) }()
 
 	b.ResetTimer()
